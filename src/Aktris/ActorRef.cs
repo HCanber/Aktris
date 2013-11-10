f9966aed1551602933dcc3a03f03720b491cea4b
HCanber <hakan_github@canberger.se>
2013-11-10 16:47:44 +0100

    Created ActorRef

---
+++ src/Aktris/ActorRef.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Aktris
+{
+	// ReSharper disable once InconsistentNaming
+	public interface ActorRef	//TODO : IComparable<ActorRef>, IEquatable<ActorRef>
+	{
+		 
+	}
+}
