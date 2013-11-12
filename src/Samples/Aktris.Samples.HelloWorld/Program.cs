51340a6b2ea4cf3657a7160c372d4f915eef4d7e
HCanber <hakan_github@canberger.se>
2013-11-12 19:11:49 +0100

    Add HelloWorld sample

---
+++ src/Samples/Aktris.Samples.HelloWorld/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aktris.Samples.HelloWorld
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var system = ActorSystem.Create();
+			var actor = system.CreateActor(ActorCreationProperties.Create<HelloWorldActor>());
+
+			actor.Send("World", null);
+
+			Console.WriteLine("Press any key");
+			Console.ReadKey();
+		}
+	}
+
+	public class HelloWorldActor : Actor
+	{
+		public HelloWorldActor()
+		{
+			Receive<string>(msg => Console.WriteLine("Hello " + msg + "!"));
+		}
+	}
+}
