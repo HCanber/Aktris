0ef9ef54b4f38bf1a16c72df5b431257d354f041
HCanber <hakan_github@canberger.se>
2013-11-16 13:23:43 +0100

    Make it possible to swap a value using Interlocked.CompareExchange and getting the value from a function.

---
+++ src/Aktris.Test/Internals/InterlockedSpin_Tests.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Aktris.Internals;
+using FluentAssertions;
+using Xunit;
+
+namespace Aktris.Test.Internals
+{
+	public class InterlockedSpin_Tests
+	{
+		[Fact]
+		public void When_a_shared_variable_is_updated_on_another_thread_Then_the_update_method_is_rerun()
+		{
+			var sharedVariable = 0;
+			var hasEnteredUpdateMethod = new ManualResetEvent(false);
+			var okToContinue = new ManualResetEvent(false);
+			var numberOfCallsToUpdateWhenSignaled = 0;
+
+			//This is what we want to test:
+			//  sharedVariable = 0
+			//  Fork to two threads:
+			//  THREAD 1                               THREAD 2
+			//                                         Call InterlockedSpin.Swap
+			//                                             It calls updateWhenSignaled(0)
+			//                                                Signal thread 1 it can do it's work, and wait for it
+			//  set sharedVariable = 42
+			//  signal thread 2 it can continue
+			//  and wait for it.
+			//                                                 return 4711 from updateWhenSignaled
+			//                                             Interlocked.CompareExchange will update sharedVariable to 4711 if it still is 0
+			//                                               which it isn't so it will fail. It will then loop.
+			//                                             Call updateWhenSignaled(42)
+			//                                                 return 4711 from updateWhenSignaled
+			//                                             Interlocked.CompareExchange will update sharedVariable to 4711 if it still is 42
+			//                                               which it is, and we return.
+			//  Test that sharedVariable=42												 
+			//  Test that updateWhenSignaled was called twice
+
+			Func<int, int> updateWhenSignaled = i =>
+			{
+				numberOfCallsToUpdateWhenSignaled++;
+				hasEnteredUpdateMethod.Set();	//Signal THREAD 1 to update sharedVariable
+				okToContinue.WaitOne(TimeSpan.FromSeconds(2));	//Wait for THREAD 1
+				return 4711;
+			};
+			var task = Task.Run(() => InterlockedSpin.Swap(ref sharedVariable, updateWhenSignaled));
+			hasEnteredUpdateMethod.WaitOne(TimeSpan.FromSeconds(2)); //Wait for THREAD 2 to enter updateWhenSignaled
+			sharedVariable = 42;
+			okToContinue.Set();	//Signal THREAD 1 it can continue in updateWhenSignaled
+			task.Wait(TimeSpan.FromSeconds(2));	//Wait for THREAD 1
+
+			sharedVariable.Should().Be(4711);
+			numberOfCallsToUpdateWhenSignaled.Should().Be(2);
+		}
+
+		[Fact]
+		public void When_a_shared_variable_is_updated_on_another_thread_Then_the_update_method_is_rerun_but_as_the_break_condition_is_fulfilled_it_do_not_update()
+		{
+			var sharedVariable = 0;
+			var hasEnteredUpdateMethod = new ManualResetEvent(false);
+			var okToContinue = new ManualResetEvent(false);
+			var numberOfCallsToUpdateWhenSignaled = 0;
+
+			//This is what we want to test:
+			//  sharedVariable = 0
+			//  Fork to two threads:
+			//  THREAD 1                               THREAD 2
+			//                                         Call InterlockedSpin.Swap
+			//                                             It calls updateWhenSignaled(0)
+			//                                                Signal thread 1 it can do it's work, and wait for it
+			//  set sharedVariable = 42
+			//  signal thread 2 it can continue
+			//  and wait for it.
+			//                                                 Since value==0 we do not want to break
+			//                                                 return <false,4711,"update"> from updateWhenSignaled
+			//                                             Interlocked.CompareExchange will update sharedVariable to 4711 if it still is 0
+			//                                               which it isn't so it will fail. It will then loop.
+			//                                             Call updateWhenSignaled(42)
+			//                                                 Since value!=0 we want to break
+			//                                                 return <true,4711,"break"> from updateWhenSignaled
+			//                                             Since first item in tuple==true, we break and return item 3: "break"
+			//  Test that sharedVariable=42												 
+			//  Test that updateWhenSignaled was called twice
+			//  Test that return from updateWhenSignaled is "break"
+
+			Func<int, Tuple<bool,int,string>> updateWhenSignaled = i =>
+			{
+				numberOfCallsToUpdateWhenSignaled++;
+				hasEnteredUpdateMethod.Set();	//Signal to start-thread that we have entered the update method (it will chang
+				okToContinue.WaitOne(TimeSpan.FromSeconds(2));	//Wait to be signalled
+				var shouldBreak = i!=0;
+				return Tuple.Create(shouldBreak,4711,shouldBreak ? "break":"update");
+			};
+			string result="";
+			var task = Task.Run(() => { result= InterlockedSpin.BreakableSwap(ref sharedVariable, updateWhenSignaled); });
+			hasEnteredUpdateMethod.WaitOne(TimeSpan.FromSeconds(2));
+			sharedVariable = 42;
+			okToContinue.Set();
+			task.Wait(TimeSpan.FromSeconds(2));
+
+			sharedVariable.Should().Be(42);
+			numberOfCallsToUpdateWhenSignaled.Should().Be(2);
+			result.Should().Be("break");
+		}
+
+	}
+
+
+}
---
+++ src/Aktris/Internals/InterlockedSpin.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace Aktris.Internals
+{
+	public static class InterlockedSpin
+	{
+		public static int Swap(ref int reference, Func<int, int> updater)
+		{
+			var spinWait = new SpinWait();
+			while(true)
+			{
+				var current = reference;
+				var updated = updater(current);
+				if(CompareExchange(ref reference, current, updated)) return updated;
+				spinWait.SpinOnce();
+			}
+		}
+
+		public static T Swap<T>(ref T reference, Func<T, T> updater) where T : class
+		{
+			var spinWait = new SpinWait();
+			while(true)
+			{
+				var current = reference;
+				var updated = updater(current);
+				if(CompareExchange(ref reference, current, updated)) return updated;
+				spinWait.SpinOnce();
+			}
+		}
+		public static TReturn BreakableSwap<T, TReturn>(ref T reference, Func<T, Tuple<bool, T, TReturn>> breakIfTrueUpdateOtherwise) where T : class
+		{
+			var spinWait = new SpinWait();
+			while(true)
+			{
+				var current = reference;
+				var t = breakIfTrueUpdateOtherwise(current);
+				if(t.Item1) return t.Item3;
+				if(CompareExchange(ref reference, current, t.Item2)) return t.Item3;
+				spinWait.SpinOnce();
+			}
+		}
+		public static TReturn BreakableSwap<TReturn>(ref int reference, Func<int, Tuple<bool, int, TReturn>> breakIfTrueUpdateOtherwise)
+		{
+			var spinWait = new SpinWait();
+			while(true)
+			{
+				var current = reference;
+				var t = breakIfTrueUpdateOtherwise(current);
+				if(t.Item1) return t.Item3;
+				if(CompareExchange(ref reference, current, t.Item2)) return t.Item3;
+				spinWait.SpinOnce();
+			}
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private static bool CompareExchange<T>(ref T reference, T expectedValue, T newValue) where T : class
+		{
+			return Interlocked.CompareExchange(ref reference, newValue, expectedValue) == expectedValue;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private static bool CompareExchange(ref int reference, int expectedValue, int newValue) 
+		{
+			return Interlocked.CompareExchange(ref reference, newValue, expectedValue) == expectedValue;
+		}
+	}
+}
