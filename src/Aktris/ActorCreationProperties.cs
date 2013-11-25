d3a579577c302e8c7dcc07d851419fa19050d29c
HCanber <hakan_github@canberger.se>
2013-11-25 21:08:34 +0100

    Create actors thru a factory

--- src/Aktris/ActorCreationProperties.cs
+++ src/Aktris/ActorCreationProperties.cs
@@ -1,4 +1,5 @@
-﻿using Aktris.Dispatching;
+﻿using System;
+using Aktris.Dispatching;
 
 namespace Aktris
 {
@@ -11,6 +12,12 @@ namespace Aktris
 		{
 			//TODO: Create this thru the ioc container instead of Activator so that dependencies can be injected. Then remove:  T: new()
 			return new DelegateActorCreationProperties(() => new T());
+		}
+
+		public static ActorCreationProperties Create<T>(Func<T> creator) where T : Actor
+		{
+			//TODO: Create this thru the ioc container instead of Activator so that dependencies can be injected.
+			return new DelegateActorCreationProperties(creator);
 		}
 
 		public abstract Mailbox CreateMailbox();
