bc1de5b4d63b58d737e281dced553d252f83c28e
HCanber <hakan_github@canberger.se>
2013-11-25 21:52:56 +0100

    Create MessageHandler that handles a specific message type

--- src/Aktris/MessageHandler.cs
+++ src/Aktris/MessageHandler.cs
@@ -1,4 +1,5 @@
 ﻿namespace Aktris
 {
 	public delegate bool MessageHandler(object message, SenderActorRef sender);
+	public delegate bool MessageHandler<in TMessage>(TMessage message, SenderActorRef sender);
 }
