4710ff1915e29861c632192d150b058889c97675
HCanber <hakan_github@canberger.se>
2013-11-25 21:15:56 +0100

    Add TestBootstrapper that uses the Synchronous Scheduler

---
+++ src/Aktris.Test/TestBootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Aktris.Dispatching;
+using Aktris.Internals;
+
+namespace Aktris.Test
+{
+	public class TestBootstrapper : IBootstrapper
+	{
+		public TestBootstrapper()
+		{
+			UniqueNameCreator = new UniqueNameCreator();
+			LocalActorRefFactory = new DefaultLocalActorRefFactory();
+			DeadLetterActorCreator = () => new DeadLetterActorRef();
+			DefaultMailboxCreator = () => new UnboundedMailbox(new SynchronousScheduler());
+		}
+		public IUniqueNameCreator UniqueNameCreator { get; private set; }
+		public LocalActorRefFactory LocalActorRefFactory { get; private set; }
+		public Func<ActorRef> DeadLetterActorCreator { get; private set; }
+		public Func<Mailbox> DefaultMailboxCreator { get; private set; }
+	}
+}
