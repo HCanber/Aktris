c9cbefc2727b0b8a5b0c672b661507c165517f3c
HCanber <hakan_github@canberger.se>
2013-11-26 21:50:55 +0100

    Add ChildrenCollection

---
+++ src/Aktris.Test/Internals/Children/ChildrenCollection_Tests.cs
@@ -0,0 +1,52 @@
+﻿using Aktris.Exceptions;
+using Aktris.Internals.Children;
+using FluentAssertions;
+using Xunit;
+
+namespace Aktris.Test.Internals.Children
+{
+// ReSharper disable once InconsistentNaming
+	public class ChildrenCollection_Tests
+	{
+		[Fact]
+		public void Reserving_succeeds()
+		{
+			var sut = EmptyChildrenCollection.Instance;
+			sut.ReserveName("Name").Should().NotBeNull();
+		}
+
+		[Fact]
+		public void Given_a_collection_where_name_has_been_reserved_and_released_Then_it_is_the_EmptyChildrenCollection()
+		{
+			var sut = EmptyChildrenCollection.Instance;
+			var coll = sut.ReserveName("Name");
+			var result = coll.ReleaseName("Name");
+			result.Should().BeSameAs(EmptyChildrenCollection.Instance);
+		}
+		[Fact]
+		public void Given_a_collection_where_name_has_been_reserved_Then_another_name_can_be_reserved()
+		{
+			var sut = EmptyChildrenCollection.Instance;
+			var coll = sut.ReserveName("Name");
+			var result = coll.ReserveName("Name2");
+		}
+		[Fact]
+		public void Given_a_collection_where_name_has_been_reserved_Then_reserving_name_again_fails()
+		{
+			var sut = EmptyChildrenCollection.Instance;
+			var coll = sut.ReserveName("Name");
+			Assert.Throws<InvalidActorNameException>(() => coll.ReserveName("Name"));
+		}
+
+		[Fact]
+		public void Given_a_collection_where_two_names_have_been_reserved_Then_the_first_name_can_be_released_and_reserved_again()
+		{
+			ChildrenCollection sut = EmptyChildrenCollection.Instance;
+			sut = sut.ReserveName("Name");
+			sut = sut.ReserveName("Name2");
+			sut = sut.ReleaseName("Name");
+			sut = sut.ReserveName("Name");
+
+		}
+	}
+}
---
+++ src/Aktris/Internals/Children/ChildrenCollection.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Immutable;
+using Aktris.Exceptions;
+
+namespace Aktris.Internals.Children
+{
+	public abstract class ChildrenCollection
+	{
+		public abstract ChildrenCollection ReserveName(string name);
+		public abstract ChildrenCollection ReleaseName(string name);
+	}
+
+	public class EmptyChildrenCollection : ChildrenCollection
+	{
+		public static readonly EmptyChildrenCollection Instance = new EmptyChildrenCollection();
+		private EmptyChildrenCollection() { }
+		public override ChildrenCollection ReserveName(string name)
+		{
+			return NormalChildrenCollection.Create(ImmutableDictionary<string, ChildInfo>.Empty.Add(name, ChildNameReserved.Instance));
+		}
+
+		public override ChildrenCollection ReleaseName(string name)
+		{
+			return this;
+		}
+	}
+
+	public class NormalChildrenCollection : ChildrenCollection
+	{
+		private readonly ImmutableDictionary<string, ChildInfo> _children;
+
+		private NormalChildrenCollection(ImmutableDictionary<string, ChildInfo> children)
+		{
+			_children = children;
+		}
+
+		public override ChildrenCollection ReserveName(string name)
+		{
+			if(_children.ContainsKey(name))
+				throw new InvalidActorNameException(string.Format("Actor name \"{0}\" is not unique!", name));
+
+			return new NormalChildrenCollection(_children.Add(name, ChildNameReserved.Instance));
+		}
+
+		public override ChildrenCollection ReleaseName(string name)
+		{
+			return Create(_children.Remove(name));
+		}
+
+		public static ChildrenCollection Create(ImmutableDictionary<string, ChildInfo> children)
+		{
+			if(children.IsEmpty) return EmptyChildrenCollection.Instance;
+			return new NormalChildrenCollection(children);
+		}
+	}
+	public interface ChildInfo { }
+
+	class ChildNameReserved : ChildInfo
+	{
+		public readonly static ChildNameReserved Instance = new ChildNameReserved();
+		private ChildNameReserved() { }
+	}
+}
