2fce09ae95253c7ef8ee7e51d865de72936e0f26
HCanber <hakan_github@canberger.se>
2013-11-27 20:42:38 +0100

    Add MinimalActorRef that pretty much do as little as possible

---
+++ src/Aktris/Internals/MinimalActorRef.cs
@@ -0,0 +1,15 @@
+﻿using Aktris.Dispatching;
+using Aktris.Internals.SystemMessages;
+
+namespace Aktris.Internals
+{
+	public abstract class MinimalActorRef : InternalActorRef
+	{
+		public abstract string Name { get; }
+		public virtual void Send(object message, ActorRef sender) {/* Intentionally left blank */}
+		public virtual void Start() {/* Intentionally left blank */}
+		public virtual void HandleMessage(Envelope envelope) {/* Intentionally left blank */}
+		public virtual void HandleSystemMessage(SystemMessageEnvelope envelope) {/* Intentionally left blank */}
+		public abstract ActorRef CreateActor(ActorCreationProperties actorCreationProperties, string name = null);
+	}
+}
