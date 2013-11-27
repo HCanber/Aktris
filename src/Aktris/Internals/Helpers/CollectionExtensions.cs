126fcd489e7e72efb71a25f350c56fc03c350efc
HCanber <hakan_github@canberger.se>
2013-11-27 11:20:31 +0100

    Add CollectionExtensions

---
+++ src/Aktris/Internals/Helpers/CollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aktris.Internals.Helpers
+{
+	public static class CollectionExtensions
+	{
+		public static void ForEach<T>(this IEnumerable<T> sequence, Action<T> action)
+		{
+			if(sequence == null) return;
+			foreach(var item in sequence)
+			{
+				action(item);
+			}
+		}
+
+		public static void ForEach<T, TArg>(this IEnumerable<T> sequence, TArg arg, Action<TArg, T> action)
+		{
+			if(sequence == null) return;
+			foreach(var item in sequence)
+			{
+				action(arg, item);
+			}
+		}
+
+	}
+}
