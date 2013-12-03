59b20322de8877b58950f3a2a6046eb6a8541a5a
HCanber <hakan_github@canberger.se>
2013-12-03 14:38:10 +0100

    Add a thread safe Random number provider

---
+++ src/Aktris/Internals/Helpers/RandomProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace Aktris.Internals.Helpers
+{
+	/// <summary>
+	/// Provides a instance of <see cref="Random"/> per thread, since <see cref="Random"/> is not thread safe.
+	/// </summary>
+	public static class RandomProvider
+	{
+		//Thanks Jon Skeet for this. http://csharpindepth.com/Articles/Chapter12/Random.aspx
+		private static int _seed = Environment.TickCount;
+
+		private static readonly ThreadLocal<Random> _RandomWrapper = new ThreadLocal<Random>(() => new Random(Interlocked.Increment(ref _seed)));
+
+		public static Random GetThreadRandom()
+		{
+			return _RandomWrapper.Value;
+		}
+	}
+}
