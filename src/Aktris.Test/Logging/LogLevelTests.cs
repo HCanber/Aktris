44eb17cfeba96e5ebe14bbaa5308f635792a4a48
HCanber <hakan_github@canberger.se>
2014-01-19 16:40:28 +0100

    Add LogLevel

---
+++ src/Aktris.Test/Logging/LogLevelTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Aktris.Logging;
+using FluentAssertions;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Aktris.Test.Logging
+{
+	public class LogLevelTests
+	{
+		[Theory,
+		InlineData(LogLevel.Off, 0),
+		InlineData(LogLevel.Error, 1),
+		InlineData(LogLevel.Warning, 2),
+		InlineData(LogLevel.Info, 3),
+		InlineData(LogLevel.Debug, 4),
+		]
+		public void Level_MustBe_Value(LogLevel logLevel, int value)
+		{
+			((int)logLevel).Should().Be(value);
+		} 
+	}
+}
---
+++ src/Aktris/Logging/LogLevel.cs
@@ -0,0 +1,11 @@
+﻿namespace Aktris.Logging
+{
+	public enum LogLevel
+	{
+		Off = 0,
+		Error = 1,
+		Warning = 2,
+		Info = 3,
+		Debug = 4,
+	}
+}
