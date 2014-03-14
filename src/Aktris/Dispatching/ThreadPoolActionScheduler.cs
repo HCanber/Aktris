e3c315b05df54d3a6e30aa2ed7ac82c4b1abf45a
HCanber <hakan_github@canberger.se>
2014-03-14 08:28:04 +0100

    Switch from Fibrous to using Task and the ordinary ThreadPool

--- src/Aktris/Dispatching/ThreadPoolActionScheduler.cs
+++ src/Aktris/Dispatching/ThreadPoolActionScheduler.cs
@@ -1,21 +1,19 @@
 ﻿using System;
-using Fibrous;
+using System.Threading.Tasks;
 
 namespace Aktris.Dispatching
 {
 	public class ThreadPoolActionScheduler : IActionScheduler
 	{
-		private readonly PoolFiber _fiber;
 
 		public ThreadPoolActionScheduler()
 		{
-			_fiber = new PoolFiber();
-			_fiber.Start();
 		}
 
 		public void Schedule(Action action)
 		{
-			_fiber.Enqueue(action);
+			// "Task is now the preferred way to queue work to the thread pool.", Eric Eilebrecht http://blogs.msdn.com/b/ericeil/archive/2009/04/23/clr-4-0-threadpool-improvements-part-1.aspx
+			Task.Run(action);
 		}
 	}
 }
