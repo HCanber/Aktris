d060d0c53f89a0868bd8907dc017ae25e107028f
HCanber <hakan_github@canberger.se>
2014-03-21 09:18:06 +0100

    Fix problems with TemporaryFileLogger

--- src/Aktris/Internals/Logging/TemporaryFileLogger.cs
+++ src/Aktris/Internals/Logging/TemporaryFileLogger.cs
@@ -1,9 +1,10 @@
 using System.IO;
 using System.Text;
+using System.Threading;
 
 namespace Aktris.Internals.Logging
 {
-	public class TemporaryFileLogger : Actor
+	public class TemporaryFileLogger : Actor	//TODO: Replace this with something better
 	{
 		public TemporaryFileLogger()
 		{
@@ -12,12 +13,15 @@ namespace Aktris.Internals.Logging
 			Receive<InitializeLogger>(_ =>
 			{
 				var exists = File.Exists("log.txt");
-				fileStream = File.OpenWrite("log.txt");
-				if(!exists)
+			
+				if(exists)
 				{
-					var preamble = Encoding.UTF8.GetPreamble();
-					fileStream.Write(preamble,0,preamble.Length);
+					File.Delete("log.txt");
 				}
+				fileStream = File.OpenWrite("log.txt");
+				var preamble = Encoding.UTF8.GetPreamble();
+				fileStream.Write(preamble, 0, preamble.Length);
+
 				Sender.Reply(LoggerInitialized.Instance);				
 			});
 			Receive<LogEvent>(e =>
